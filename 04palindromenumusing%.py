828d402a40f81eac4b0a90033f93d9d10b8f3318
manishpatil55 <155523680+manishpatil55@users.noreply.github.com>
2024-01-07 13:29:35 +0530

    palindrome num using % method

--- 04palindromenumusing%.py
+++ 04palindromenumusing%.py
@@ -5,7 +5,7 @@ def palindrome(n):
     for _ in range(len(str(n))):
         last_digit = n % 10         # 12321 % 10 = 1    i.e last_digit = 1
         rev_num = (rev_num * 10) + last_digit   # rev_num = (0 * 10) + 1    so now rev_num = 1
-        n //= 10 # it makes the number n = 12321 to 1232.1 then removes the point and assigns the remaining value to n   i.e now n = 1232
+        n /= 10 # it makes the number n = 12321 to 1232.1 then removes the point and assigns the remaining value to n   i.e now n = 1232
 
                         # The loop runs the number of times equal to the length of the input number
     return num == rev_num
